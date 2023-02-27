4adf3a2862cc5a1402fe8f02097123ea1eadac7f
Vitor Barbeiro <vitor.agb@hotmail.com>
2023-02-27 07:00:39 -0300

    Send Files

---
+++ README.md
@@ -0,0 +1,10 @@
+# About the project
+
+Academic project for the Javascript course at B7Web.
+Site for ordering pizza; just uses a portion of JS to display the information about the pizza and allows users to select the quantity and flavor to be sent to the shopping cart.
+
+# Technologies used
+## Front end
+- HTML
+- CSS   
+- JavaScript
---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:[14.58, 17.16, 20.19], sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:[13.00, 15.30, 18.00], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:[12.60, 14.83, 17.45], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:[14.28, 16.80, 19.77], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:[15.48, 18.21, 21.43], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:[13.40, 15.76, 18.55], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:[16.15, 19.00, 22.36], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+];
